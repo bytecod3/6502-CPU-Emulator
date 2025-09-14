efb6abf11f7818f1d249f15d519523d3f4726290
bytecod3 <emwiti658@gmail.com>
2025-09-14 05:05:36 +0300

    FIXES CMAKE lists

--- include/cpu.h
+++ include/cpu.h
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "types.h"
+#include "memory-map.h"
 
 /* status register */
 typedef struct sr {
@@ -19,9 +20,111 @@ typedef struct  {
     uint8_t AC;     /* accumulator */
     uint8_t X;      /* index register X */
     uint8_t Y;      /* index register Y */
-    status_register_t  SR;             /* status register */
+    status_register_t SR;             /* status register */
     uint8_t SP;     /* stack pointer */
+
+    uint32_t cycles;
 } CPU_type_t;
 
+/* addressing modes */
+enum addressing_modes {
+    ABSOLUTE_A = 0,
+    ABSOLUTE_INDEXED_INDIRECT,
+    ABSOLUTE_INDEXED_WITH_X,
+    ABSOLUTE_INDEXED_WITH_Y,
+    ABSOLUTE_INDIRECT,
+    ACCUMULATOR,
+    IMMEDIATE_ADDRESSING,
+    IMPLIED,
+    PROGRAM_COUNTER_RELATIVE,
+    STACK,
+    ZERO_PAGE,
+    ZERO_PAGE_INDEXED_INDIRECT,
+    ZERO_PAGE_INDEXED_WITH_X,
+    ZERO_PAGE_INDEXED_WITH_Y,
+    ZERO_PAGE_INDIRECT,
+    ZERO_PAGE_INDIRECT_INDEXED_WITH_Y,
+};
+
+/* opcode matrix table  */
+//uint8_t OPCODES[16][16] = {
+//        {}
+//};
+
+/* opcodes */
+enum OPCODES {
+    ADC,
+    AND,
+    ASL,
+    DCC,
+    BCS,
+    BEQ,
+    BIT,
+    BMI,
+    BNE,
+    BPL,
+    BRA,
+    BRK,
+    BVC,
+    CLC,
+    CLD,
+    CLI,
+    CLV,
+    CMP,
+    CPX,
+    CPY,
+    DEC,
+    DEX,
+    DEY,
+    EOR,
+    INC,
+    INX,
+    INY,
+    JMP,
+    JSR,
+    LDA,
+    LDX,
+    LDY,
+    LSR,
+    NOP,
+    CRA,
+    PHA,
+    PHP,
+    PHX,
+    PHY,
+    PLA,
+    PLP,
+    PLX,
+    PLY,
+    ROL,
+    ROR,
+    RTI,
+    RTS,
+    SBC,
+    SEC,
+    SED,
+    SEI,
+    STA,
+    STX,
+    STY,
+    STZ,
+    TAX,
+    TAY,
+    TRB,
+    TSB,
+    TSX,
+    TZA,
+    TXS,
+    TYA
+};
+
+// todo:assuming immediate mode addressing
+struct instruction {
+
+};
+
+uint8_t opcode;
 void cpu_reset(CPU_type_t*);
+void cpu_fetch_instruction(CPU_type_t*, uint16_t* address);
+void cpu_decode_instruction();
 
--- src/cpu.c
+++ src/cpu.c
@@ -1,9 +1,10 @@
 #include "cpu.h"
 #include "memory-map.h"
+#include <stdint.h>
 
 /**
  * store the reset memory addresses.
- * the stack will always reside in teh address space of 0x0100 - 0x01FF
+ * the stack will always reside in the address space of 0x0100 - 0x01FF
  * we only need to store the LSB since this address will always start with 0x01
  * Around 7 cycles are used to get the SP to 0xFF but since I am not
  * emulating a cycle accurate CPU, I jump to the final values of the registers
@@ -17,3 +18,12 @@ void cpu_reset(CPU_type_t* cpu) {
     cpu->X = 0x00;
     cpu->Y = 0x00;
 }
+
+/**
+ * fetch instruction from memory at the address pointed to by PC
+ * effectively, increment the PC so it points to the next address
+ * decrement the number of CPU cycles needed to fetch this instruction
+ */
+//void cpu_fetch_instruction(CPU_type_t* cpu) {
+//    uint8_t opcode =
+//}
